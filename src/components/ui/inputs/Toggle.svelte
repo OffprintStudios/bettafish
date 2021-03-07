23daaa85639b0b027ef4545ae745225d77014d94
Alyx Mote <figments@outlook.com>
2021-03-06 22:35:53 -0800

    simplified toggle

--- src/components/ui/inputs/Toggle.svelte
+++ src/components/ui/inputs/Toggle.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
     export let value: boolean = false;
     export let label: string;
-
-    function switchValue() {
-        value = !value;
-    }
 </script>
 
 <div class="flex flex-row items-center mt-3 mb-3">
-    <div class="switch-container w-12 h-6 flex items-center rounded-full" on:click={switchValue}>
+    <div class="switch-container w-12 h-6 flex items-center rounded-full" on:click={() => value = !value}>
         <div
             class="switch w-4 h-4 rounded-full shadow-md transform duration-300 ease-in-out"
             class:switch-activated={value}
@@ -23,11 +19,9 @@
         background: transparent;
         padding: 0.2rem;
         cursor: pointer;
-
         div.switch {
             background: gray;
         }
-
         div.switch-activated {
             background: var(--accent);
             --tw-translate-x: 1.5rem/* 24px */;
